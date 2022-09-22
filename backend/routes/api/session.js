a30240d3cf28ed8e2b9e046f8b482dab2a342266
Christo Grabowski <christo.grab@gmail.com>
2022-09-22 17:10:47 -0500

    Added User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,6 +5,7 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+// Log in
 router.post('/', async (req, res, next) => {
   const { credential, password } = req.body;
   
@@ -22,6 +23,12 @@ router.post('/', async (req, res, next) => {
 
   return res.json({ user });
   
+});
+
+// Log out
+router.delete('/', async (req, res) => {
+  res.clearCookie('token');
+  return res.json({ message: 'success' })
 });
 
 module.exports = router;
