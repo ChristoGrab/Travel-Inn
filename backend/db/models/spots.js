5129052aad63b2a30198a72fef2a974ec599dd9a
Christo Grabowski <christo.grab@gmail.com>
2022-09-23 16:09:57 -0500

    added initial Spots model/migration

---
+++ backend/db/models/spots.js
@@ -0,0 +1,54 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Spots extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Spots.init({
+    ownerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true
+    },
+    address: {
+      type: DataTypes.STRING
+    },
+    city: {
+      type: DataTypes.STRING
+    },
+    state: {
+      type: DataTypes.STRING
+    },
+    country: {
+      type: DataTypes.STRING
+    },
+    lat: {
+      type: DataTypes.DECIMAL
+    },
+    lng: {
+      type: DataTypes.DECIMAL
+    },
+    name: {
+      type: DataTypes.STRING
+    },
+    description: {
+      type: DataTypes.STRING
+    },
+    price: {
+      type: DataTypes.DECIMAL
+    }
+  }, {
+    sequelize,
+    modelName: 'Spots',
+  });
+  return Spots;
+};
