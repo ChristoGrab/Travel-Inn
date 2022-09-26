1bf2c41babbfbf0d5148269d9b1b8799a3010876
Christo Grabowski <christo.grab@gmail.com>
2022-09-25 23:01:45 -0500

    edited foreignKeys in model associations

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Review.belongsTo(models.User, { foreignKey: 'userId' })
       Review.belongsTo(models.Spot, { foreignKey: 'spotId' })
+      Review.hasMany(models.ReviewImage, { foreignKey: 'reviewId'})
     }
   }
   Review.init({
--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      SpotImage.belongsTo(models.Spot, { foreignKey: spotId })
+      SpotImage.belongsTo(models.Spot, { foreignKey: 'spotId' })
     }
   }
   SpotImage.init({
