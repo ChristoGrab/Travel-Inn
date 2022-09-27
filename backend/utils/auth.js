b7607ec927b26e559a8d3fe19fc9ec79c57cf24c
Christo Grabowski <christo.grab@gmail.com>
2022-09-27 14:59:28 -0500

    Changed wording of Authentication error message

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -54,9 +54,8 @@ const setTokenCookie = (res, user) => {
 const requireAuth = function (req, _res, next) {
   if (req.user) return next();
 
-  const err = new Error('Unauthorized');
-  err.title = 'Unauthorized';
-  err.errors = ['Unauthorized'];
+  const err = new Error('Authentication required');
+  err.title = 'Authentication required';
   err.status = 401;
   return next(err);
 }
