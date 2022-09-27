a35055c71cf383df7110727708656f9e1f3a9ca1
Christo Grabowski <christo.grab@gmail.com>
2022-09-27 17:51:26 -0500

    Added more validation errors on User signup

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -11,19 +11,26 @@ const validateSignup = [
   check('email')
     .exists({ checkFalsy: true })
     .isEmail()
-    .withMessage('Please provide a valid email.'),
+    .withMessage('Invalid email'),
   check('username')
     .exists({ checkFalsy: true })
     .isLength({ min: 4 })
     .withMessage('Please provide a username with at least 4 characters.'),
   check('username')
     .not()
+    .exists({ checkFalsy: true })
     .isEmail()
-    .withMessage('Username cannot be an email.'),
+    .withMessage('Username is required and cannot be an email.'),
   check('password')
     .exists({ checkFalsy: true })
     .isLength({ min: 6 })
     .withMessage('Password must be 6 characters or more.'),
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage("First Name is required"),
+  check('lastName')
+    .exists({ checkFalsey: true })
+    .withMessage("Last Name is required"),
   handleValidationErrors
 ];
 
