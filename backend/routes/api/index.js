e1d70622844c24f1cbb9658726c00fcd0a5d231d
Christo Grabowski <christo.grab@gmail.com>
2022-09-27 10:50:42 -0500

    Added logged in user tests

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -10,11 +10,19 @@ const { restoreUser } = require('../../utils/auth.js');
   // If current user session is not valid, set req.user to null
 router.use(restoreUser);
 
-router.use('/session', sessionRouter);
 
+// List of routers
+router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 
+
+//Test for logged in user
+router.get('/test', requireAuth, (req, res) => {
+  res.send("Success!")
+})
+
+//Test endpoint for post method
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
