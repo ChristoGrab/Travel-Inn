5f571ffb8e20681e3749ab6818b40227723567c9
Christo Grabowski <christo.grab@gmail.com>
2022-09-26 22:57:17 -0500

    Working on including previewImg and avgRating in response

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,29 +1,47 @@
 const express = require('express')
-const { Spot } = require('../../db/models');
-const { Review } = require('../../db/models')
+const { Spot, Review, SpotImage } = require('../../db/models');
 const router = express.Router();
 const { Op } = require('sequelize');
-const sequelize = require('sequelize')
+const sequelize = require('sequelize');
 
-// GET all spots
+
+
+
+// GET all Spots
 
 router.get('/', async (req, res, next) => {
+  
   const allSpots = await Spot.findAll({
-    attributes: {
-      include: [
-        [
-          sequelize.fn("AVG", sequelize.col("Reviews.stars")),
-          "avgStars"
-        ]
-      ]
-    },
-    include: {
+    include: [
+      {
       model: Review,
       attributes: []
+    },
+    {
+      model: SpotImage
     }
+  ]
   });
   
-  res.json({"Spots": allSpots});
+  // Create an array of each spot
+  let spotsList = [];
+  allSpots.forEach(spot => {
+    spotsList.push(spot.toJSON())
+  })
+
+  
+  spotsList.forEach(spot => {
+    spot.SpotImages.forEach(image => {
+      if (image.preview === true) {
+        spot.previewImage = image.url
+      }
+    })
+    if (!spot.previewImage) {
+      spot.previewImage = "This spot doesn't have a preview"
+    }
+  })
+  
+  res.json({spotsList});
 })
 
 module.exports = router;
