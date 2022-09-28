e89abdd849321a7c0027413aabd960675a63afad
Christo Grabowski <christo.grab@gmail.com>
2022-09-28 12:57:20 -0500

    added all functionality for adding image to review endpoint other than max limit check

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -29,4 +29,52 @@ router.get('/current', requireAuth, async (req, res) => {
   return res.json({"Reviews": myReviews})
 });
 
+// ADD AN IMAGE TO A REVIEW BASED ON THE REVIEW'S ID
+router.post('/:reviewId/images', requireAuth, async (req, res) => {
+  const review = await Review.findByPk(req.params.reviewId, {
+    include: [
+      {
+        model: ReviewImage,
+        attributes: []
+      }
+    ]
+  })
+  
+  // Return 404 if review doesn't exist
+  if (!review) {
+    res.status(404)
+    return res.json({
+      "message": "Review couldn't be found",
+      "statusCode": 404
+    })
+  }
+  
+  // Return 403 if review belongs to a different user
+  if (review.userId !== req.user.id) {
+    res.status(403)
+    return res.json({
+      "message": "Forbidden",
+      "statusCode": 403
+    })
+  }
+  
+  // Return 403 if review already has 10 images
+  // if (review.ReviewImage.length >= 10) {
+  //   res.status(403)
+  //   return res.json({
+  //     "message": "Maximum number of images for this resource was reached",
+  //     "statusCode": 403
+  //   })
+  // } 
+  
+  const { url } = req.body
+  
+  const newImage = await ReviewImage.create({
+    reviewId: req.params.reviewId,
+    url
+  })
+
+  return res.json(newImage)
+})
+
 module.exports = router;
