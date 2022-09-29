27ffee8ce86c2177cc272c04962636eed5447267
Christo Grabowski <christo.grab@gmail.com>
2022-09-29 10:52:48 -0500

    Fully implemented endpoint to delete a SpotImage by Id

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,7 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
 const bookingsRouter = require('./bookings.js');
+const spotImageRouter = require('./spot-images.js');
 const { restoreUser, requireAuth } = require('../../utils/auth.js');
 
 
@@ -19,6 +20,7 @@ router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
 router.use('/bookings', bookingsRouter);
+router.use('/spot-images', spotImageRouter);
 
 
 //Test for logged in user
