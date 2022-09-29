974a1608f69fb9162bfc10c4bff854f173086df9
Christo Grabowski <christo.grab@gmail.com>
2022-09-29 18:18:53 -0500

    Added validation errors for creating or editing spot

--- backend/utils/errors.js
+++ backend/utils/errors.js
@@ -10,6 +10,40 @@ const validateReview = [
     .isInt({ min: 1, max: 5 })
     .withMessage('Please provide a whole number between 1 and 5'),
     handleValidationErrors
+];
+
+const validateSpot = [
+  check('address')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Street address is required'),
+  check('city')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('City is required'),
+  check('state')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('State is required'),
+  check('country')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Country is required'),
+  check('description')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Description is required'),
+  check('price')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Price per day is required'),
+  handleValidationErrors
+];
+
+const bookingsValidate = [
+  check('endDate')
+    .isAfter()
+    .withMessage("endDate cannot be on or before startDate")
 ]
 
 
