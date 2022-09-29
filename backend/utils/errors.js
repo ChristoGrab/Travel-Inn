5ca97c51c4e82b0253b045d5266d2fdf9e93254c
Christo Grabowski <christo.grab@gmail.com>
2022-09-28 21:43:50 -0500

    created validation errors for creating reviews

---
+++ backend/utils/errors.js
@@ -0,0 +1,15 @@
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('./validation');
+
+const validateReview = [
+  check('review')
+    .exists({ checkFalsy: true})
+    .notEmpty()
+    .withMessage('Review text is required'),
+  check('stars')
+    .isInt({ min: 1, max: 5 })
+    .withMessage('Please provide a whole number between 1 and 5'),
+    handleValidationErrors
+]
+
+module.exports = { validateReview };
