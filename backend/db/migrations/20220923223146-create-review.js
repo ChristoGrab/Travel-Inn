482e6d4d7aad5cec534c3bd4477a9243e913698a
Christo Grabowski <christo.grab@gmail.com>
2022-09-30 16:16:30 -0500

    small edit to review migration to comply with postgres data types

--- backend/db/migrations/20220923223146-create-review.js
+++ backend/db/migrations/20220923223146-create-review.js
@@ -19,7 +19,7 @@ module.exports = {
         allowNull: false
       },
       stars: {
-        type: Sequelize.INTEGER(1)
+        type: Sequelize.INTEGER
       },
       createdAt: {
         allowNull: false,
