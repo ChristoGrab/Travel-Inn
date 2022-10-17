e9d8cab11dbb497a7388a2aeab8053af8022de36
Christo Grabowski <christo.grab@gmail.com>
2022-10-17 16:26:10 -0500

    minor edits to skeleton of ViewSpotDetails component

--- frontend/src/components/CreateSpotForm/index.js
+++ frontend/src/components/CreateSpotForm/index.js
@@ -1,18 +1,15 @@
-import { Redirect } from 'react-router-dom';
 import { useHistory } from 'react-router-dom'
 
 function CreateSpotButton() {
   
   const history = useHistory()
-  
+
   const displaySpotForm = (e) => {
     e.preventDefault();
-    
-    console.log("I am clicked")
-    
+
     history.push('/create')
   }
-  
+
   return (
     <button id="create-spot-button" 
     onClick={displaySpotForm}>
--- frontend/src/components/ViewSpotDetails/index.js
+++ frontend/src/components/ViewSpotDetails/index.js
@@ -1,7 +1,8 @@
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { getOneSpot } from '../../store/spots'
+import { getOneSpot } from '../../store/spots';
+import './ViewSpotDetails.css';
 
 function ViewSpotDetails() {
   const dispatch = useDispatch();
@@ -12,7 +13,8 @@ function ViewSpotDetails() {
   }, [dispatch, spotId])
 
   const mySpot = useSelector(state => state.spots.singleSpot);
-  
+  const currentUser = useSelector(state => state.session.user)
+
   let imageList = []
   
   // need to create a conditional for mapping images, otherwise
@@ -27,15 +29,15 @@ function ViewSpotDetails() {
   return (
     <div className="spot-details-container">
       <div className="spot-details-header">
-        {mySpot.name}
-        {mySpot.avgRating}
-        {mySpot.numReviews}
-        {mySpot.city}, {mySpot.state}, {mySpot.country}
+        <div>{mySpot.name}</div>
+        <div>{mySpot.avgRating}</div>
+        <div>{mySpot.numReviews}</div>
+        <div>{mySpot.city}, {mySpot.state}, {mySpot.country}</div>
       </div>
       <div className="spot-details-image-list">
         <ul>
           {imageList.map(img => (
-            <img key ={img.id} src={img.url}></img>
+            <img key ={img.id} src={img.url} alt={img.name}></img>
           ))}
         </ul>
       </div>
