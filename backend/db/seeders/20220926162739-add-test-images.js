e93ae3fd0c74a4a6521b85bcdfdde4b522e36e4a
Christo Grabowski <christo.grab@gmail.com>
2022-10-18 18:36:12 -0500

    added real image urls to seeder files in backend

--- backend/db/seeders/20220926162739-add-test-images.js
+++ backend/db/seeders/20220926162739-add-test-images.js
@@ -14,27 +14,27 @@ module.exports = {
    await queryInterface.bulkInsert('SpotImages', [
     {
       spotId: 1,
-      url: 'https://i.picsum.photos/id/1040/4496/3000.jpg?hmac=kvZONlBpTcZ16PuE_g2RWxlicQ5JKVq2lqqZndfafBY',
+      url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-39084966/original/e2e38afa-e0eb-43bd-8621-592bea7595b0.jpeg?im_w=720',
       preview: true
     },
     {
       spotId: 1,
-      url: 'image2.url',
+      url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-39084966/original/87fc9417-1158-43cb-bb02-2b6f04d3c487.jpeg?im_w=720',
       preview: false
     },
     {
       spotId: 2,
-      url: 'https://i.picsum.photos/id/164/1200/800.jpg?hmac=wkqGUkaeW3kiAsHq_VwxSWWossIMAwFV4eUfFzuDkew',
+      url: 'https://a0.muscache.com/im/pictures/miso/Hosting-52854046/original/30c02e83-4e1f-4e91-8cfa-f988eb9ec1d0.jpeg?im_w=720',
       preview: true
     },
     {
       spotId: 2,
-      url: 'image4.url',
+      url: 'https://a0.muscache.com/im/pictures/miso/Hosting-52854046/original/6dd8550f-907a-4c68-9876-a7051116d27c.jpeg?im_w=720',
       preview: false
     },
     {
       spotId: 3,
-      url: 'https://i.picsum.photos/id/215/3264/2448.jpg?hmac=u-g_DAEnfO1tx6Y9o80X1qzqC7rpmPHJ94JhqVa8SCI',
+      url: 'https://a0.muscache.com/im/pictures/8829b15a-2169-4c81-b35c-8f4125d0d18f.jpg?im_w=720',
       preview: true
     }    
    ])
