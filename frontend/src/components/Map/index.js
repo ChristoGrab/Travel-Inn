003ad8d22ecf52efa2ec7da7a640cf25a3311125
Christo Grabowski <christo.grab@gmail.com>
2023-01-13 12:04:39 -0600

    building Maps API

--- frontend/src/components/Map/index.js
+++ frontend/src/components/Map/index.js
@@ -1,18 +1,23 @@
-const Map = ( {lat, lng} ) => {
-  
-  
-  
-  return (
-    <iframe
-  className="spot-page-map"
-  width="80%"
-  height="450px"
-  loading="lazy"
-  allowFullScreen
-  referrerPolicy="no-referrer-when-downgrade"
-  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDG-PUmSFM0AoQe7tuf7BrSUfiTGGQ2eUM&q=Space+Needle,Seattle+WA">
-</iframe>
-  )
-}
+import GoogleMapReact from 'google-map-react';
+
+const Map = ({ location, zoomLevel }) => (
+  <div className="map">
+    <h2 className="map-h2">Come Visit Us At Our Campus</h2>
+
+    <div className="google-map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: '' }}
+        defaultCenter={location}
+        defaultZoom={zoomLevel}
+      >
+        <LocationPin
+          lat={location.lat}
+          lng={location.lng}
+          text={location.address}
+        />
+      </GoogleMapReact>
+    </div>
+  </div>
+)
 
 export default Map;
