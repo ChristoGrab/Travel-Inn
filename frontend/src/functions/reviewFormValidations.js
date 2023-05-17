dbf913d87807e1706662ffff40592ba4bfd8378f
Christo Grabowski <christo.grab@gmail.com>
2023-05-17 01:10:34 -0400

    add comments to frontend helper functions for clarity

--- frontend/src/functions/reviewFormValidations.js
+++ frontend/src/functions/reviewFormValidations.js
@@ -1,3 +1,8 @@
+// This function is used to validate the review form. It checks if the user has provided a rating and a review.
+// It also checks if the review contains any inappropriate language.
+// If any of these conditions are not met, the function returns an array of errors.
+// If all conditions are met, the function returns an empty array.
+
 export const reviewFormValidations = (stars, review) => {
 
   let errors = []
